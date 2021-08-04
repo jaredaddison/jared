420615f5ce08b108f4937bce1c62ca3567097b6e
Jared Addison <77221122+jaredaddison@users.noreply.github.com>
2021-08-04 22:40:12 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
 import { useHead } from '@vueuse/head'
 
-// https://github.com/vueuse/head
-// you can use this to manipulate the document head in any components,
-// they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'VETSupport',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'Saving Pets & Training Vets' },
   ],
 })
 </script>
